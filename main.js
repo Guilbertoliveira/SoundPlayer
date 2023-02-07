2835542e67fb86f7665cb7653d1670d3d67c6e77
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-02-07 15:08:13 -0300

    project update

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
+document.querySelectorAll('.tecla');
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
 
