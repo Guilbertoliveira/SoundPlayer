1d9801b83c6c60007ea5fa96223e0adf34772bab
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-02-07 21:03:45 -0300

    javascript update

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElemento){
+    document.querySelector(idElemento).play();
 }
 
-document.querySelectorAll('.tecla');
 //document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
 
-
-
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    };
+    console.log(contador);
+    contador++;
+}
 
